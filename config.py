82a5d4c551bf299b976c05ff9604d49e66f7759e
Madpychosxop <xyzgaming155@gmail.com>
2024-07-23 14:15:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,19 +1,16 @@
 # Don't use quotes( " and ' )
-#SCRIPT BY VENOMxCRAZY
+#SCRIPT BY MADPYCHOS16
   
 #Enter Your Bot Token here get it from @botfarher
 BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("MADPYCHOS16")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("6694202933")
 
 
 
 
 
-
-
-  
