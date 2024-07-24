e64384704216125b1c20aeb0fa250ee57ea4ba11
Madpychosxop <xyzgaming155@gmail.com>
2024-07-24 19:15:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,10 +5,10 @@
 BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("MADPYCHOS16")
+OWNER_USERNAME=("ronitplayzzz")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("6694202933")
+ADMIN_IDS=("6057904321")
 
 
 
